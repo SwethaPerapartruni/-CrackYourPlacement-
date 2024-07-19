b890fdbbe919f3b9c6d15f20d225076d17d9e12e
SwethaPerapartruni <151494844+SwethaPerapartruni@users.noreply.github.com>
2024-07-19 17:08:23 +0530

    Day 1

---
+++ Find_the_Duplicate_Number.java
@@ -0,0 +1,26 @@
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//         HashSet<Integer> hashSet = new HashSet<>();
+//         for (int i = 0; i < nums.length; i++) {
+//             if (hashSet.contains(nums[i])) {
+//                 return nums[i];
+//             } else {
+//                 hashSet.add(nums[i]);
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int n = nums.length;
+        boolean[] set = new boolean[n+1];
+        for(int i=0;i<n;i++) {
+            if(set[nums[i]]) return nums[i];
+            set[nums[i]] = true;
+        }
+        return -1;
+    }
+}
