140868775f9ea8d5bf3ff582cce6b0970b0107c9
SwethaPerapartruni <151494844+SwethaPerapartruni@users.noreply.github.com>
2024-07-21 14:18:49 +0530

    Day 6

---
+++ Implement_Stack_using_Queues.java
@@ -0,0 +1,40 @@
+class MyStack {
+    private Queue<Integer> queue1;
+    private Queue<Integer> queue2;
+
+    public MyStack() {
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        queue2.offer(x);//x to q2
+        while (!queue1.isEmpty()) {
+            queue2.offer(queue1.poll());// all elements to q1 to q2
+        }
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+    }
+    
+    public int pop() {
+        return queue1.poll();// remove the front element of the que
+    }
+    
+    public int top() {
+        return queue1.peek();
+    }
+    
+    public boolean empty() {
+        return queue1.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
